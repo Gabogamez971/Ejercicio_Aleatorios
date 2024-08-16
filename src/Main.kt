d0501c01784245cc0c0f981ee55a84239c1b5090
Gabriel Gamez <gamezgabriel971@gmail.com>
2024-08-16 15:36:12 -0500

    Moneda

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,22 @@
+import kotlin.random.Random
+
 fun main() {
-    val name = "Kotlin"
-    println("Aprendiendo comandos")
+    var cont : Int= 0
+    var moneda : Int
+
+    var puntosCara : Int =0
+    var puntosSello : Int =0
+    while (cont<20){
+        moneda = Random.nextInt(1,3)
+        if (moneda==1){
+            puntosCara++
+            cont++
+            println("Cara Gano")
+        }else if(moneda==2){
+            puntosSello++
+            cont++
+            println("Sello gano")
+        }
+    }
+    println("Puntos de cara: $puntosCara y Puntos de sellos: $puntosSello")
 }
